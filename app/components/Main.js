8d5b79dcd83a9d45eea85df542d9bb501525d87d
Kristen Fanning <kristen.fanning@northwestern.edu>
2017-06-30 21:20:45 -0500

    function shells and form

---
+++ app/components/Main.js
@@ -0,0 +1,28 @@
+var React = require("react");
+// Import Children
+var Saved = require("./children/Saved");
+var Search = require("./children/Search");
+
+// Import Helper Functions
+var helpers = require("./utils/helper");
+
+// Create Main Component
+var Main = React.createClass({
+	getInitialState:function(){
+
+	},
+	componentDidMount: function(){
+
+	},
+	componentDidUpdate: function(){
+
+	}, 
+	setTerm: function(){
+
+	},
+	render: function(){
+		
+	}
+});
+
+module.exports = Main;
---
+++ app/components/children/Saved.js
@@ -0,0 +1,7 @@
+var React = require("react");
+
+// Create Saved Component
+var Saved = React.createClass({
+
+});
+module.exports = Saved;
